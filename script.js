07f7bef5ba73bc7190400ddbafb8632c139163d8
23-shivamsingh <shivams2307@gmail.com>
2025-01-26 02:23:27 +0530

    Added URL

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json";
+const BASE_URL =  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2025.1.25/v1/currencies"
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn =  document.querySelector("form button");
+const toCurr = document.querySelector(".to select");
+const fromCurr = document.querySelector(".from select");
+const msg = document.querySelector(".message");
 
 for (let select of dropdowns) {
     for (currCode in countryList) {
@@ -29,7 +32,7 @@ const updateFlag = (element) => {
     image.src = newSrc;
 }
 
-btn.addEventListener("click", (evt) => {
+btn.addEventListener("click", async (evt) => {
     evt.preventDefault();
     let amount = document.querySelector(".amount input");
     let amtVal = amount.value;
@@ -37,4 +40,10 @@ btn.addEventListener("click", (evt) => {
         amtVal = 1;
         amount.value = "1";
     }
+
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[toCurr.value.toLowerCase()];
+    console.log(rate);
 });
